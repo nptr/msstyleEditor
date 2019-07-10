6bc94ef6427bc13d0644830e9574f25d158d671b
Jakob <jjk@outlook.at>
2019-07-10 21:17:15 +0200

    added basic command line support (#31)

--- msstyleEditor/main.cpp
+++ msstyleEditor/main.cpp
@@ -16,5 +16,13 @@ bool wxMyApp::OnInit()
 	MainWindow* frame = new MainWindow(NULL, wxID_ANY, wxT("msstyleEditor"), wxDefaultPosition, wxSize(900, 600));
 	frame->Show(true);
 
+	if (wxApp::argc > 1)
+	{
+		if (wxFile::Exists(wxApp::argv[1]))
+		{
+			frame->OpenStyle(wxApp::argv[1]);
+		}
+	}
+
 	return true;
 }
